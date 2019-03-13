f3c48ba3fac7ca07439dafc8a8991bd6f81a5abc
Barak Tawily <quitten11@gmail.com>
2019-03-13 14:29:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -88,10 +88,9 @@ class ItemEnterEventListener(EventListener):
             extension.show_notification("Error", "Check the logs")
             raise
 
-    def killall(self, extension, keyword):
-        cmd = ['killall', keyword]
+    def killall(self, extension, data):
+        cmd = ['killall', data['cmd']]
         logger.info(' '.join(cmd))
-
         try:
             check_call(cmd) == 0
             extension.show_notification("Done", "It's dead now", icon=dead_icon)
@@ -122,7 +121,7 @@ class ItemEnterEventListener(EventListener):
             return self.show_signal_options(data)
         else:
             if data['keyword'] == 'killall':
-                self.killall(extension, data['argument'])
+                self.killall(extension, data)
             if data['keyword'] == 'kill':
                 self.kill(extension, data['pid'], data.get('signal', 'TERM'))
 
